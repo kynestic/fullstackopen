9dbaa0c7086c14574ec47be99b7b9b80dec02881
Nguyen Manh Duc <muadong1198@gmail.com>
2024-08-05 22:15:38 +0700

    update

--- part3/part3-testing/index.js
+++ part3/part3-testing/index.js
@@ -82,6 +82,5 @@ app.put('/api/notes/:id' , (request, response) => {
   response.status(200).json(newNote)
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
